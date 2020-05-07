94e324c8da8da2e5bebaf207e3254ae10867e349
princeryadav <prince.yadavp098@gmail.com>
2020-05-07 15:10:58 +0530

    commetn added in client file

--- client.js
+++ client.js
@@ -1,10 +1,13 @@
+// Checking input value 
+// is positive number.
 function f1(m,n){
     if(m>0 && n>0){
         return true;
     }
     return false;
 }
-
+// Add input value and
+// return result
 function f2(a,b){
     if(f1(a,b)){
         let c= a+b;
@@ -12,10 +15,12 @@ function f2(a,b){
     }
     return undefined;
 }
-
+// Printing return value 
+// of f2 function
 function f3(){
     let x=5;
     let y=9;
   console.log(f2(x,y));
 }
+// Invoking function f3
 f3();  
